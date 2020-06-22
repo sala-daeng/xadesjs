2b3555e97b479aa3a8a05fb9cdce586ce8172a21
saranpol <saran@sala-daeng.com>
2020-06-23 04:21:34 +0700

    remove fix id

--- src/signed_xml.ts
+++ src/signed_xml.ts
@@ -135,10 +135,10 @@ export class SignedXml extends XmlDSigJs.SignedXml {
             throw new XmlCore.XmlError(XmlCore.XE.XML_EXCEPTION, "Cannot create QualifyingProperties cause current signature has got one. You must create CounterSignature");
         }
 
-        // const rnd = XmlDSigJs.Application.crypto.getRandomValues(new Uint8Array(6)) as Uint8Array;
-        // const id = XmlCore.Convert.ToHex(rnd);
-        console.log('fix id to bf909aaa-c8f6-4420-bc33-e5171067fda7')
-        const id = 'bf909aaa-c8f6-4420-bc33-e5171067fda7'
+        const rnd = XmlDSigJs.Application.crypto.getRandomValues(new Uint8Array(6)) as Uint8Array;
+        const id = XmlCore.Convert.ToHex(rnd);
+        // console.log('fix id to bf909aaa-c8f6-4420-bc33-e5171067fda7')
+        // const id = 'bf909aaa-c8f6-4420-bc33-e5171067fda7'
 
         // this.XmlSignature.Id = `id-${id}`;
         this.XmlSignature.Id = `xmldsig-${id}`;
